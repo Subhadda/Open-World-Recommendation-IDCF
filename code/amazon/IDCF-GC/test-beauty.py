fa73c496214ed7a8610bfdf208ff6960a9baf798
Subhadda <155903441+Subhadda@users.noreply.github.com>
2021-07-24 11:25:48 +0000

    inital

---
+++ code/amazon/IDCF-GC/test-beauty.py
@@ -0,0 +1,53 @@
+
+import os
+import time
+import random
+import pickle
+import numpy as np
+import pandas as pd
+import argparse
+import yaml
+from model import IRMC_GC_Model, GCMCModel
+from utils import *
+from datetime import datetime
+import torch
+import torch.nn.functional as F
+
+def fix_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+fix_seed(1234)
+
+parser = argparse.ArgumentParser(description='PMF')
+parser.add_argument('--gpus', default='0', help='gpus')
+parser.add_argument('--extra', action="store_true", help='whether extra or inter')
+args = parser.parse_args()
+
+config = yaml.safe_load(open("./datainfo.yaml", 'r'))
+
+os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
+device = torch.device('cuda')
+
+LEARNING_RATE = 0.001
+DECAYING_FACTOR = 1.
+LAMBDA_REG = 0.05
+BATCH_SIZE_TRAIN = 1024
+BATCH_SIZE_TEST = 1024
+HIS_MAXLEN = 100
+HIS_SAMPLE_NUM = 20
+n_epochs = 1 # 500
+
+DATASET = 'amazon-beauty'
+EXTRA = args.extra
+
+datadir = '../../../data/beauty_s20.pkl'
+train_set_supp, train_set_que, test_set_supp, test_set_que, user_his_dic, user_supp_list, edge_UI = \
+generate_data(datadir=datadir, sample_graph=True)
+config = yaml.safe_load(open("./datainfo.yaml", 'r'))
+n_user = config[DATASET]['n_user']
+n_item = config[DATASET]['n_item']
+
+test_set_supp = torch.tensor(test_set_supp)
