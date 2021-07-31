840537b0347d9174d0589e4b1c86cea5522625f7
Subhadda <155903441+Subhadda@users.noreply.github.com>
2021-07-31 14:38:07 +0000

    Lines authored by marcocova

--- code/amazon/IDCF-GC/test-books.py
+++ code/amazon/IDCF-GC/test-books.py
@@ -23,3 +23,64 @@ def fix_seed(seed):
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
 fix_seed(1234)
+
+parser = argparse.ArgumentParser(description='PMF')
+parser.add_argument('--gpus', default='0', help='gpus')
+parser.add_argument('--extra', action="store_true", help='whether extra or inter')
+args = parser.parse_args()
+
+config = yaml.safe_load(open("./datainfo.yaml", 'r'))
+
+os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
+device = torch.device('cuda')
+
+LEARNING_RATE = 0.001
+DECAYING_FACTOR = 1.
+LAMBDA_REG = 0.05
+BATCH_SIZE_TRAIN = 1024
+BATCH_SIZE_TEST = 1024
+HIS_MAXLEN = 100
+HIS_SAMPLE_NUM = 20
+n_epochs = 1 # 500
+
+DATASET = 'amazon-books'
+EXTRA = args.extra
+
+datadir = '../../../data/books_s20.pkl'
+train_set_supp, train_set_que, test_set_supp, test_set_que, user_his_dic, user_supp_list, edge_UI = \
+generate_data(datadir=datadir, sample_graph=True)
+config = yaml.safe_load(open("./datainfo.yaml", 'r'))
+n_user = config[DATASET]['n_user']
+n_item = config[DATASET]['n_item']
+
+test_set_supp = torch.tensor(test_set_supp)
+test_set_que = torch.tensor(test_set_que)
+supp_users = torch.tensor(user_supp_list, dtype = torch.long)
+edge_sparse = torch.tensor(edge_UI)
+
+user_supp_num = len(user_supp_list)
+user_que_num = n_user - user_supp_num
+
+def sequence_adjust(seq):
+	seq_new = seq
+	if len(seq) <= 0:
+		seq_new = [np.random.randint(0, n_item) for i in range(HIS_SAMPLE_NUM)]
+	if len(seq) > HIS_MAXLEN:
+		random.shuffle(seq)
+		seq_new = seq[:HIS_MAXLEN]
+	return seq_new
+
+def neg_sampling(train_set_i, num_neg_per = 5):
+	size = train_set_i.size(0)
+	neg_iid = torch.randint(0, n_item, (num_neg_per * size, )).reshape(-1)
+	return torch.stack([train_set_i[:, 0].repeat(num_neg_per), neg_iid, torch.zeros(num_neg_per * size)], dim=1)
+
+def test(model, test_set, supp_or_que):
+	model.eval()
+	loss_r_test_sum, l1_sum, l2_sum = 0., 0., 0.
+	test_size = test_set.size(0)
+	user_score_dict, user_label_dict = {}, {}
+	for k in user_his_dic.keys():
+		user_score_dict[k] = []
+		user_label_dict[k] = []
+	score_label = []
