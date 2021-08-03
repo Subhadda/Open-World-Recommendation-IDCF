ee243b403fd5d12008a976ed00b0b5e4d1f98efc
Subhadda <155903441+Subhadda@users.noreply.github.com>
2021-08-03 14:59:26 +0000

    v2.7.0

--- code/amazon/IDCF-GC/test-books.py
+++ code/amazon/IDCF-GC/test-books.py
@@ -84,3 +84,62 @@ def test(model, test_set, supp_or_que):
 		user_score_dict[k] = []
 		user_label_dict[k] = []
 	score_label = []
+	for i in range(test_size // BATCH_SIZE_TEST + 1):
+		with torch.no_grad():
+			test_set_i = test_set[i*BATCH_SIZE_TEST : (i+1)*BATCH_SIZE_TEST]
+			test_set_neg_i = neg_sampling(test_set_i)
+			test_set_i = torch.cat([test_set_i, test_set_neg_i], dim=0)
+			test_set_i_x = test_set_i[:, :2].long().to(device)
+			test_set_i_y = test_set_i[:, 2].float().to(device)
+			test_set_his_i = [torch.tensor(
+				sequence_adjust( user_his_dic[test_set_i[k][0].item()] ),
+				dtype = torch.long
+				)   for k in range(test_set_i.size(0))]
+			test_set_hl_i = [test_set_his_i[k].size(0) for k in range(test_set_i.size(0))]
+			test_set_his_i = torch.nn.utils.rnn.pad_sequence(test_set_his_i, batch_first = True, padding_value = 0.).to(device)
+			test_set_hl_i = torch.tensor(test_set_hl_i, dtype=torch.long).to(device)
+
+			if supp_or_que == 'supp':
+				pred_y = model(test_set_i_x)
+			else:
+				pred_y = model(test_set_i_x, test_set_his_i, test_set_hl_i)
+			loss_r = F.binary_cross_entropy_with_logits(pred_y, test_set_i_y, reduction='sum')
+		y_hat, y = pred_y.cpu().numpy().tolist(), test_set_i_y.cpu().numpy().tolist()
+		for i in range(len(y)):
+			score_label.append([y_hat[i], y[i]])
+		loss_r_test_sum += loss_r
+		for k in range(test_set_i.size(0)):
+			u, s, y = test_set_i_x[k, 0].item(), pred_y[k].item(), test_set_i_y[k].item()
+			user_score_dict[u] += [s]
+			user_label_dict[u] += [y]
+	score_label = sorted(score_label, key=lambda d:d[0], reverse=True)
+	ndcg_sum, num = 0., 0
+	for k in user_score_dict.keys():
+		if len(user_score_dict[k]) <= 1:
+			continue
+		ndcg_sum += ndcg_k(user_score_dict[k], user_label_dict[k], len(user_score_dict[k]))
+		num += 1
+	print(num)
+	return score_label, ndcg_sum, num
+
+def load_model_s(model, path):
+	model.load_model(path+'model.pkl')
+
+def load_model_q(model, path):
+	if EXTRA:
+		model.load_model(path + 'model-extra.pkl')
+	else:
+		model.load_model(path + 'model-inter.pkl')
+
+if EXTRA:
+	model_q = IRMC_GC_Model(n_user = n_user,
+				n_item = n_item,
+				supp_users = supp_users,
+				device = device,
+				edge_sparse=edge_sparse,).to(device)
+	load_model_q(model_q, './train-books/')
+
+	score_label_q, ndcg_sum_q, num_q = test(model_q, test_set_que, supp_or_que='que')
+	AUC_q = auc_calc(score_label_q)
+	NDCG_q = ndcg_sum_q / num_q
+	log = 'Que Test Result AUC_q: {:.4f} NDCG_q: {:.4f}'.format(AUC_q, NDCG_q)
