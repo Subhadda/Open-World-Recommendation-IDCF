22413c5251c98aab198e2d63dcc2ab2c886b0ed8
Subhadda <155903441+Subhadda@users.noreply.github.com>
2021-08-05 07:46:25 +0000

    Update changes.md

--- code/amazon/IDCF-GC/train-beauty.py
+++ code/amazon/IDCF-GC/train-beauty.py
@@ -87,3 +87,71 @@ def auc_calc(score_label):
         return 1 - auc / (tp2 * fp2)
     except:
         return 0.5
+
+def train(model, optimizer, i):
+	model.train()
+	optimizer.zero_grad()
+	
+	train_set_que_i = train_set[i*BATCH_SIZE_TRAIN : (i+1)*BATCH_SIZE_TRAIN]
+	train_set_que_neg_i = neg_sampling(train_set_que_i)
+	train_set_que_i = torch.cat([train_set_que_i, train_set_que_neg_i], dim=0)
+	train_set_i_x = train_set_que_i[:, :2].long().to(device)
+	train_set_i_y = train_set_que_i[:, 2].float().to(device)
+	train_set_his_i = [torch.tensor(
+		sequence_adjust( user_his_dic[train_set_que_i[k][0].item()] ),
+		dtype = torch.long
+		)   for k in range(train_set_que_i.size(0))]
+	train_set_hl_i = [train_set_his_i[k].size(0) for k in range(train_set_que_i.size(0))]
+	train_set_his_i = torch.nn.utils.rnn.pad_sequence(train_set_his_i, batch_first = True, padding_value = 0.).to(device)
+	train_set_hl_i = torch.tensor(train_set_hl_i, dtype=torch.long).to(device)
+	if EXTRA:
+		pred_y, user_emb_ind, user_emb_trd = model(train_set_i_x, train_set_his_i, train_set_hl_i, mode='EXTRA')
+		loss = F.binary_cross_entropy_with_logits(pred_y, train_set_i_y, reduction='sum')
+		user_emb_trd_ = user_emb_trd.unsqueeze(0).repeat(user_emb_ind.size(0), 1, 1)
+		user_emb_ind_ = user_emb_ind.unsqueeze(1).repeat(1, user_emb_trd.size(0), 1)
+		dot_prod = torch.sum(torch.mul(user_emb_trd_, user_emb_ind_), dim=-1)
+		loss_con = - torch.mean(
+					dot_prod.diagonal() - torch.logsumexp(dot_prod, dim=-1)
+				)
+		loss += 10.0 * loss_con
+	else:
+		pred_y = model(train_set_i_x, train_set_his_i, train_set_hl_i)
+		loss = F.binary_cross_entropy_with_logits(pred_y, train_set_i_y, reduction='sum')
+	loss.backward()
+	optimizer.step()
+	return loss.item(), 0.
+
+def test(model, test_set):
+	model.eval()
+	loss_r_test_sum = 0
+	score_label = []
+	test_size = test_set.size(0)
+	for i in range(test_size // BATCH_SIZE_TEST + 1):
+		with torch.no_grad():
+			test_set_i = test_set[i*BATCH_SIZE_TEST : (i+1)*BATCH_SIZE_TEST]
+			test_set_neg_i = neg_sampling(test_set_i)
+			test_set_i = torch.cat([test_set_i, test_set_neg_i], dim=0)
+			test_set_i_x = test_set_i[:, :2].long().to(device)
+			test_set_i_y = test_set_i[:, 2].float().to(device)
+			test_set_his_i = [torch.tensor(
+				sequence_adjust( user_his_dic[test_set_i[k][0].item()] ),
+				dtype = torch.long
+			)   for k in range(test_set_i.size(0))]
+			test_set_hl_i = [test_set_his_i[k].size(0) for k in range(test_set_i.size(0))]
+			test_set_his_i = torch.nn.utils.rnn.pad_sequence(test_set_his_i, batch_first = True, padding_value = 0.).to(device)
+			test_set_hl_i = torch.tensor(test_set_hl_i, dtype=torch.long).to(device)
+
+			pred_y = model(test_set_i_x, test_set_his_i, test_set_hl_i, sample_graph=True)
+			loss_r = F.binary_cross_entropy_with_logits(pred_y, test_set_i_y, reduction='sum')
+		y_hat, y = pred_y.cpu().numpy().tolist(), test_set_i_y.cpu().numpy().tolist()
+		for i in range(len(y)):
+			score_label.append([y_hat[i], y[i]])
+		loss_r_test_sum += loss_r
+	score_label = sorted(score_label, key=lambda d:d[0], reverse=True)
+	loss_r_test = loss_r_test_sum / test_size
+	AUC = auc_calc(score_label)
+
+	return loss_r_test.item(), AUC
+
+def save_model(model, path):
+	if EXTRA:
