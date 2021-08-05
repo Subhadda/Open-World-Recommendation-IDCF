420cb94f801a820cbb06a5d76cfb2e22292e8451
Subhadda <155903441+Subhadda@users.noreply.github.com>
2021-08-05 06:03:24 +0000

    getting up to date

--- code/amazon/IDCF-GC/train-beauty.py
+++ code/amazon/IDCF-GC/train-beauty.py
@@ -33,3 +33,57 @@ device = torch.device('cuda')
 
 DATASET = 'amazon-beauty'
 LEARNING_RATE = 0.0001 # EXTRA 0.00001
+DECAYING_FACTOR = 1.
+LAMBDA_REC = 1.
+BATCH_SIZE_TRAIN = 1024
+BATCH_SIZE_TEST = 1024
+HIS_MAXLEN = 100
+HIS_SAMPLE_NUM = 20
+n_epochs = 500 # 500
+EXTRA = False
+
+datadir = '../../../data/beauty_s20.pkl'
+train_set_supp, train_set_que, test_set_supp, test_set_que, user_his_dic, user_supp_list, edge_UI = \
+generate_data(datadir=datadir, sample_graph=True)
+config = yaml.safe_load(open("./datainfo.yaml", 'r'))
+n_user = config[DATASET]['n_user']
+n_item = config[DATASET]['n_item']
+
+if EXTRA:
+	train_set = torch.tensor(train_set_supp)
+else:
+	train_set = torch.tensor(train_set_que)
+test_set = torch.tensor(test_set_que)
+supp_users = torch.tensor(user_supp_list, dtype = torch.long)
+edge_sparse = torch.tensor(edge_UI)
+
+train_set = train_set[torch.randperm(train_set.size(0))]
+val_set = train_set[int(0.95*train_set.size(0)):]
+train_set = train_set[:int(0.95*train_set.size(0))]
+
+def sequence_adjust(seq):
+	seq_new = seq
+	if len(seq) <= 0:
+		seq_new = [np.random.randint(0, n_item) for i in range(HIS_SAMPLE_NUM)]
+	if len(seq) > HIS_MAXLEN:
+		random.shuffle(seq)
+		seq_new = seq[:HIS_MAXLEN]
+	return seq_new
+
+def neg_sampling(train_set_i, num_neg_per = 1):
+	size = train_set_i.size(0)
+	neg_iid = torch.randint(0, n_item, (num_neg_per * size, )).reshape(-1)
+	return torch.stack([train_set_i[:, 0].repeat(num_neg_per), neg_iid, torch.zeros(num_neg_per * size)], dim=1)
+	#return train_set_i
+
+def auc_calc(score_label):
+    fp1, tp1, fp2, tp2, auc = 0.0, 0.0, 0.0, 0.0, 0.0
+    for s in score_label:
+        fp2 += (1-s[1]) # noclick
+        tp2 += s[1] # click
+        auc += (tp2 - tp1) * (fp2 + fp1) / 2
+        fp1, tp1 = fp2, tp2
+    try:
+        return 1 - auc / (tp2 * fp2)
+    except:
+        return 0.5
