95bbd2e0dd56e3cf65e3ee6ef5e5a8a85916c15d
Subhadda <155903441+Subhadda@users.noreply.github.com>
2021-08-06 17:17:01 +0000

    pagination

--- code/amazon/IDCF-GC/train-books.py
+++ code/amazon/IDCF-GC/train-books.py
@@ -69,3 +69,66 @@ def sequence_adjust(seq):
 		random.shuffle(seq)
 		seq_new = seq[:HIS_MAXLEN]
 	return seq_new
+
+def neg_sampling(train_set_i, num_neg_per = 1):
+	size = train_set_i.size(0)
+	neg_iid = torch.randint(0, n_item, (num_neg_per * size, )).reshape(-1)
+	return torch.stack([train_set_i[:, 0].repeat(num_neg_per), neg_iid, torch.zeros(num_neg_per * size)], dim=1)
+	#return train_set_i
+
+def auc_calc(score_label):
+    fp1, tp1, fp2, tp2, auc = 0.0, 0.0, 0.0, 0.0, 0.0
+    for s in score_label:
+        fp2 += (1-s[1]) # noclick
+        tp2 += s[1] # click
+        auc += (tp2 - tp1) * (fp2 + fp1) / 2
+        fp1, tp1 = fp2, tp2
+    try:
+        return 1 - auc / (tp2 * fp2)
+    except:
+        return 0.5
+
+def train(model, optimizer, i):
+	model.train()
+	optimizer.zero_grad()
+	
+	train_set_que_i = train_set[i*BATCH_SIZE_TRAIN : (i+1)*BATCH_SIZE_TRAIN]
+	train_set_que_neg_i = neg_sampling(train_set_que_i)
+	train_set_que_i = torch.cat([train_set_que_i, train_set_que_neg_i], dim=0)
+	train_set_i_x = train_set_que_i[:, :2].long().to(device)
+	train_set_i_y = train_set_que_i[:, 2].float().to(device)
+	train_set_his_i = [torch.tensor(
+		sequence_adjust( user_his_dic[train_set_que_i[k][0].item()] ),
+		dtype = torch.long
+		)   for k in range(train_set_que_i.size(0))]
+	train_set_hl_i = [train_set_his_i[k].size(0) for k in range(train_set_que_i.size(0))]
+	train_set_his_i = torch.nn.utils.rnn.pad_sequence(train_set_his_i, batch_first = True, padding_value = 0.).to(device)
+	train_set_hl_i = torch.tensor(train_set_hl_i, dtype=torch.long).to(device)
+	if EXTRA:
+		pred_y, user_emb_ind, user_emb_trd = model(train_set_i_x, train_set_his_i, train_set_hl_i, mode='EXTRA')
+		loss = F.binary_cross_entropy_with_logits(pred_y, train_set_i_y, reduction='sum')
+		user_emb_trd_ = user_emb_trd.unsqueeze(0).repeat(user_emb_ind.size(0), 1, 1)
+		user_emb_ind_ = user_emb_ind.unsqueeze(1).repeat(1, user_emb_trd.size(0), 1)
+		dot_prod = torch.sum(torch.mul(user_emb_trd_, user_emb_ind_), dim=-1)
+		loss_con = - torch.mean(
+					dot_prod.diagonal() - torch.logsumexp(dot_prod, dim=-1)
+				)
+		loss += 10.0 * loss_con
+	else:
+		pred_y = model(train_set_i_x, train_set_his_i, train_set_hl_i)
+		loss = F.binary_cross_entropy_with_logits(pred_y, train_set_i_y, reduction='sum')
+	loss.backward()
+	optimizer.step()
+	return loss.item(), 0.
+
+def test(model, test_set):
+	model.eval()
+	loss_r_test_sum = 0
+	score_label = []
+	test_size = test_set.size(0)
+	for i in range(test_size // BATCH_SIZE_TEST + 1):
+		with torch.no_grad():
+			test_set_i = test_set[i*BATCH_SIZE_TEST : (i+1)*BATCH_SIZE_TEST]
+			test_set_neg_i = neg_sampling(test_set_i)
+			test_set_i = torch.cat([test_set_i, test_set_neg_i], dim=0)
+			test_set_i_x = test_set_i[:, :2].long().to(device)
