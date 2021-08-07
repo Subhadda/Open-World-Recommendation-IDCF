77de568b435703d9e63707c8597a3e7ffe46f6f3
Subhadda <155903441+Subhadda@users.noreply.github.com>
2021-08-07 18:23:18 +0000

    More README

---
+++ code/amazon/IDCF-GC/utils.py
@@ -0,0 +1,27 @@
+
+import pickle
+import random
+import yaml
+import numpy as np
+
+random.seed(1234)
+
+def generate_data(datadir, sample_graph=True):
+    try:
+        with open(datadir, 'rb') as f:
+            ucs_set = pickle.load(f)
+            cs_set = pickle.load(f)
+            u_his_list = pickle.load(f)
+            i_his_list = pickle.load(f)
+            ucs_count, cs_count, item_count = pickle.load(f)
+    except:
+        with open(datadir, 'rb') as f:
+            ucs_set = pickle.load(f)
+            cs_set = pickle.load(f)
+            ucs_count, cs_count, item_count = pickle.load(f)
+
+    user_supp_num, user_que_num = ucs_count, cs_count
+    train_set_supp, test_set_supp = [], []
+    train_set_que, test_set_que = [], []
+
+    user_supp_list = [u for u in range(ucs_count)]
