3d8b979567beabf4facc35892d47b5dcb4716190
Subhadda <155903441+Subhadda@users.noreply.github.com>
2021-09-04 17:39:30 +0000

    *facepalm*

--- code/amazon/IDCF-NN/test-beauty.py
+++ code/amazon/IDCF-NN/test-beauty.py
@@ -22,4 +22,7 @@ fix_seed(1234)
 
 parser = argparse.ArgumentParser(description='PMF')
 parser.add_argument('--gpus', default='0', help='gpus')
-parser.add_argument('--extra', action="store_true", help='whether extra or inte
+parser.add_argument('--extra', action="store_true", help='whether extra or inter')
+args = parser.parse_args()
+
+config = yaml.safe_load(ope
