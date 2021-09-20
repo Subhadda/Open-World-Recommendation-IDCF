394af7b5ce52beb534d037b591f20087ea270992
Subhadda <155903441+Subhadda@users.noreply.github.com>
2021-09-20 10:00:30 +0000

    merge from subversion client

--- code/amazon/IDCF-NN/test-beauty.py
+++ code/amazon/IDCF-NN/test-beauty.py
@@ -60,4 +60,5 @@ def sequence_adjust(seq):
 
 def neg_sampling(train_set_i, num_neg_per = 5):
 	size = train_set_i.size(0)
-	neg_iid = torch.randint(0, n_item, (num_neg_per * 
+	neg_iid = torch.randint(0, n_item, (num_neg_per * size, )).reshape(-1)
+	return torch.stack([train_set_i[
