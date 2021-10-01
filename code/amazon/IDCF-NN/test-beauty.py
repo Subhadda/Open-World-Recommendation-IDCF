46c3b799bc565fb764f550ef9fc856284940b9d7
Subhadda <155903441+Subhadda@users.noreply.github.com>
2021-10-01 19:52:35 +0000

    Inline variable

--- code/amazon/IDCF-NN/test-beauty.py
+++ code/amazon/IDCF-NN/test-beauty.py
@@ -79,4 +79,5 @@ def test(model, test_set, supp_or_que):
 			test_set_i_x = test_set_i[:, :2].long().to(device)
 			test_set_i_y = test_set_i[:, 2].float().to(device)
 			test_set_his_i = [torch.tensor(
-				sequence_adjus
+				sequence_adjust( user_his_dic[test_set_i[k][0].item()] ),
+				dtype = torch
