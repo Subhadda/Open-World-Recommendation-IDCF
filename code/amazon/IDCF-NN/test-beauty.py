5e5289992c246a409c5be000cf768a04f103a6b5
Subhadda <155903441+Subhadda@users.noreply.github.com>
2021-10-06 19:46:16 +0000

    Update webpack.config.js

--- code/amazon/IDCF-NN/test-beauty.py
+++ code/amazon/IDCF-NN/test-beauty.py
@@ -90,4 +90,4 @@ def test(model, test_set, supp_or_que):
 				pred_y = model(test_set_i_x)
 			else:
 				pred_y = model(test_set_i_x, test_set_his_i, test_set_hl_i)
-		y_hat
+		y_hat, y = pred_y.cpu().numpy().tolist(), test_set_i_y.cpu().num
