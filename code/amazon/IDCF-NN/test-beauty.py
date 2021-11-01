5bbad1757bd6b168b71a21c93da0ec4a52482ad3
Subhadda <155903441+Subhadda@users.noreply.github.com>
2021-11-01 07:03:12 +0000

    ALSA: hda - Fix polarity of mute LED on HP Mini 210

--- code/amazon/IDCF-NN/test-beauty.py
+++ code/amazon/IDCF-NN/test-beauty.py
@@ -125,4 +125,5 @@ if EXTRA:
 
 	score_label_q, ndcg_sum_q, num_q = test(model_q, test_set_que, supp_or_que='que')
 	AUC_q = auc_calc(score_label_q)
-	
+	NDCG_q = ndcg_sum_q / num_q
+	log = 'Que Test Resu
