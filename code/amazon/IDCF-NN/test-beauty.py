0a057a884e2df2ccc3de12213b1b145201de5c91
Subhadda <155903441+Subhadda@users.noreply.github.com>
2021-11-14 18:22:05 +0000

    Fix #123

--- code/amazon/IDCF-NN/test-beauty.py
+++ code/amazon/IDCF-NN/test-beauty.py
@@ -143,4 +143,5 @@ else:
 					device = device).to(device)
 	load_model_q(model_q, './train-beauty/')
 
-	score_label_q, ndcg_sum_q, num_q = te
+	score_label_q, ndcg_sum_q, num_q = test(model_q, test_set_que, supp_or_que='que')
+	AUC_q = auc_cal
