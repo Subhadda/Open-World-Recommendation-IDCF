434ea23e2a744942e61619915de6801dca54469b
Subhadda <155903441+Subhadda@users.noreply.github.com>
2021-11-21 21:38:55 +0000

    better layout

--- code/amazon/IDCF-NN/test-books.py
+++ code/amazon/IDCF-NN/test-books.py
@@ -39,3 +39,56 @@ HIS_SAMPLE_NUM = 20
 EXTRA = args.extra
 
 DATASET = 'amazon-books'
+datadir = '../../../data/books_s20.pkl'
+train_set_supp, train_set_que, test_set_supp, test_set_que, user_his_dic, user_supp_list = generate_data(datadir=datadir)
+config = yaml.safe_load(open("./datainfo.yaml", 'r'))
+n_user = config[DATASET]['n_user']
+n_item = config[DATASET]['n_item']
+
+test_set_supp = torch.tensor(test_set_supp)
+test_set_que = torch.tensor(test_set_que)
+supp_users = torch.tensor(user_supp_list, dtype = torch.long)
+
+user_supp_num = len(user_supp_list)
+user_que_num = n_user - user_supp_num
+
+def sequence_adjust(seq):
+	seq_new = seq
+	if len(seq) <= 0:
+		seq_new = [np.random.randint(0, n_item) for i in range(HIS_SAMPLE_NUM)]
+	if len(seq) > HIS_MAXLEN:
+		random.shuffle(seq)
+		seq_new = seq[:HIS_MAXLEN]
+	return seq_new
+
+def neg_sampling(train_set_i, num_neg_per = 5):
+	size = train_set_i.size(0)
+	neg_iid = torch.randint(0, n_item, (num_neg_per * size, )).reshape(-1)
+	return torch.stack([train_set_i[:, 0].repeat(num_neg_per), neg_iid, torch.zeros(num_neg_per * size)], dim=1)
+
+def test(model, test_set, supp_or_que):
+	model.eval()
+	test_size = test_set.size(0)
+	user_score_dict, user_label_dict = {}, {}
+	for k in user_his_dic.keys():
+		user_score_dict[k] = []
+		user_label_dict[k] = []
+	score_label = []
+	for i in range(test_size // BATCH_SIZE_TEST + 1):
+		with torch.no_grad():
+			test_set_i = test_set[i*BATCH_SIZE_TEST : (i+1)*BATCH_SIZE_TEST]
+			test_set_neg_i = neg_sampling(test_set_i)
+			test_set_i = torch.cat([test_set_i, test_set_neg_i], dim=0)
+			test_set_i_x = test_set_i[:, :2].long().to(device)
+			test_set_i_y = test_set_i[:, 2].float().to(device)
+			test_set_his_i = [torch.tensor(
+				sequence_adjust( user_his_dic[test_set_i[k][0].item()] ),
+				dtype = torch.long
+				)   for k in range(test_set_i.size(0))]
+			test_set_hl_i = [test_set_his_i[k].size(0) for k in range(test_set_i.size(0))]
+			test_set_his_i = torch.nn.utils.rnn.pad_sequence(test_set_his_i, batch_first = True, padding_value = 0.).to(device)
+			test_set_hl_i = torch.tensor(test_set_hl_i, dtype=torch.long).to(device)
+
+			if supp_or_que == 'supp':
+				pred_y = model(test_set_i_x)
+			else:
