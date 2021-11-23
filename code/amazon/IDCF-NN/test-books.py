f5b213a546f144208c7f30beaeb6876d908d34ee
Subhadda <155903441+Subhadda@users.noreply.github.com>
2021-11-23 14:53:37 +0000

    Setup travis-ci

--- code/amazon/IDCF-NN/test-books.py
+++ code/amazon/IDCF-NN/test-books.py
@@ -92,3 +92,69 @@ def test(model, test_set, supp_or_que):
 			if supp_or_que == 'supp':
 				pred_y = model(test_set_i_x)
 			else:
+				pred_y = model(test_set_i_x, test_set_his_i, test_set_hl_i)
+		y_hat, y = pred_y.cpu().numpy().tolist(), test_set_i_y.cpu().numpy().tolist()
+		for i in range(len(y)):
+			score_label.append([y_hat[i], y[i]])
+		for k in range(test_set_i.size(0)):
+			u, s, y = test_set_i_x[k, 0].item(), pred_y[k].item(), test_set_i_y[k].item()
+			user_score_dict[u] += [s]
+			user_label_dict[u] += [y]
+	score_label = sorted(score_label, key=lambda d:d[0], reverse=True)
+	ndcg_sum, num = 0., 0
+	for k in user_score_dict.keys():
+		if len(user_score_dict[k]) <= 1:
+			continue
+		ndcg_sum += ndcg_k(user_score_dict[k], user_label_dict[k], len(user_score_dict[k]))
+		num += 1
+	return score_label, ndcg_sum, num
+
+def load_model_s(model, path):
+	model.load_model(path+'model.pkl')
+
+def load_model_q(model, path):
+	if EXTRA:
+		model.load_model(path + 'model-extra.pkl')
+	else:
+		model.load_model(path+'model-inter.pkl')
+
+
+if EXTRA:
+	model_q = IRMC_NN_Model(n_user=n_user,
+							n_item=n_item,
+							supp_users=supp_users,
+							device=device).to(device)
+	load_model_q(model_q, './train-books/')
+
+	score_label_q, ndcg_sum_q, num_q = test(model_q, test_set_que, supp_or_que='que')
+	AUC_q = auc_calc(score_label_q)
+	NDCG_q = ndcg_sum_q / num_q
+	log = 'Que Test Result AUC_q: {:.4f} NDCG_q: {:.4f}'.format(AUC_q, NDCG_q)
+	print(log)
+else:
+	model_s = NNMFModel(n_user, n_item).to(device)
+	load_model_s(model_s, './pretrain-books/')
+	score_label_s, ndcg_sum_s, num_s = test(model_s, test_set_supp, supp_or_que='supp')
+	AUC_s = auc_calc(score_label_s)
+	NDCG_s = ndcg_sum_s / num_s
+	log = 'Supp Test Result AUC_s: {:.4f} NDCG_s: {:.4f}'.format(AUC_s, NDCG_s)
+	print(log)
+
+	model_q = IRMC_NN_Model(n_user = n_user,
+					n_item = n_item,
+					supp_users = supp_users,
+					device = device).to(device)
+	load_model_q(model_q, './train-books/')
+
+	score_label_q, ndcg_sum_q, num_q = test(model_q, test_set_que, supp_or_que='que')
+	AUC_q = auc_calc(score_label_q)
+	NDCG_q = ndcg_sum_q / num_q
+	log = 'Que Test Result AUC_q: {:.4f} NDCG_q: {:.4f}'.format(AUC_q, NDCG_q)
+	print(log)
+
+	score_label = score_label_s + score_label_q
+	score_label = sorted(score_label, key=lambda d:d[0], reverse=True)
+	AUC = auc_calc(score_label)
+	NDCG = ( ndcg_sum_s + ndcg_sum_q )/ (num_s+num_q)
+	log = 'All Test Result AUC: {:.4f} NDCG: {:.4f}'.format(AUC, NDCG)
+	print(log)
