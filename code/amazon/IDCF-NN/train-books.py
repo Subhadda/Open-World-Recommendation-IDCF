df53125c3eb1e1a59a33bdcf9aaf956bee0ec4d5
Subhadda <155903441+Subhadda@users.noreply.github.com>
2021-12-01 07:27:15 +0000

    Small Updates

--- code/amazon/IDCF-NN/train-books.py
+++ code/amazon/IDCF-NN/train-books.py
@@ -3,3 +3,47 @@ import os
 import time
 import random
 import pickle
+import numpy as np
+import pandas as pd
+import argparse
+import yaml
+from model import IRMC_NN_Model
+from utils import *
+from datetime import datetime
+import torch
+import torch.nn.functional as F
+
+import logging
+logging.basicConfig(level=logging.INFO, filename='record.log', format='%(message)s')
+
+def fix_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+fix_seed(1234)
+
+parser = argparse.ArgumentParser(description='PMF')
+parser.add_argument('--gpus', default='0', help='gpus')
+args = parser.parse_args()
+
+os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
+device = torch.device('cuda')
+
+DATASET = 'amazon-books'
+LEARNING_RATE = 0.01
+DECAYING_FACTOR = 1.
+LAMBDA_REC = 1.
+BATCH_SIZE_TRAIN = 256
+BATCH_SIZE_TEST = 1024
+HIS_MAXLEN = 100
+HIS_SAMPLE_NUM = 20
+n_epochs = 500 # 500
+EXTRA = False
+
+datadir = '../../../data/books_s20.pkl'
+train_set_supp, train_set_que, test_set_supp, test_set_que, user_his_dic, user_supp_list = generate_data(datadir=datadir)
+config = yaml.safe_load(open("./datainfo.yaml", 'r'))
+n_user = config[DATASET]['n_user']
+n_item = config[DATASET]['n_item']
