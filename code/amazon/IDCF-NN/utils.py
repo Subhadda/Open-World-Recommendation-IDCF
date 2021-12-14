f82380753f60d8eeb8d6c6cc322e2cfde4ef3f8c
Subhadda <155903441+Subhadda@users.noreply.github.com>
2021-12-14 11:15:54 +0000

    lein-release plugin: preparing 0.1.1 release

--- code/amazon/IDCF-NN/utils.py
+++ code/amazon/IDCF-NN/utils.py
@@ -11,4 +11,6 @@ def generate_data(datadir):
     try:
         with open(datadir, 'rb') as f:
             ucs_set = pickle.load(f)
-            cs_set = pickle.load(f
+            cs_set = pickle.load(f)
+            u_his_list = pickle.load(f)
+            i_his_list = 
