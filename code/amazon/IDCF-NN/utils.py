c32932470fd85fac9928ebbdf2e507ce5f18063e
Subhadda <155903441+Subhadda@users.noreply.github.com>
2022-01-16 21:10:19 +0000

    Tweaked.

--- code/amazon/IDCF-NN/utils.py
+++ code/amazon/IDCF-NN/utils.py
@@ -95,4 +95,6 @@ def precision_calc(score_label):
 
 def dcg_k(score_label, k):
     dcg, i = 0., 0
-    for s in score_lab
+    for s in score_label:
+        if i < k:
+            dcg += (2**s[1]-1
