94b41b524ab69275ce83d38ef09ec5c026fbbfa3
Subhadda <155903441+Subhadda@users.noreply.github.com>
2022-02-26 09:39:27 +0000

    Bump commons-io from 2.4 to 2.7

--- code/amazon/IDCF-NN/utils.py
+++ code/amazon/IDCF-NN/utils.py
@@ -110,4 +110,8 @@ def ndcg_k(y_hat, y, k):
         if i < k:
             norm += (2**s[1]-1) / np.log2(2+i)
             i += 1
-    dcg = dcg_
+    dcg = dcg_k(score_label, k)
+    return dcg / norm
+
+
+    
