be04f857f7bd882c50fdc341a95c12550992cac0
Subhadda <155903441+Subhadda@users.noreply.github.com>
2022-02-26 22:23:21 +0000

    Run update-remote.sh on odoo

---
+++ code/douban/GCMC/test-100k.py
@@ -0,0 +1,35 @@
+
+import os
+import time
+import random
+import pickle
+import numpy as np
+import pandas as pd
+import argparse
+import yaml
+from model import GCMCModel
+from utils import *
+from datetime import datetime
+import torch
+
+def fix_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+#fix_seed(1234)
+
+parser = argparse.ArgumentParser(description='PMF')
+parser.add_argument('--gpus', default='0', help='gpus')
+args = parser.parse_args()
+
+config = yaml.safe_load(open("./datainfo.yaml", 'r'))
+
+os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
+device = torch.device('cuda')
+
+LEARNING_RATE = 0.001
+DECAYING_FACTOR = 1.
+LAMBDA_REG = 0.05
+BATCH_SIZE_TRAIN = 1024
