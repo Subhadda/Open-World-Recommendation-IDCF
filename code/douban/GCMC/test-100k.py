a0da8cc257e35214cd8ee7ee3619fb3531c05c01
Subhadda <155903441+Subhadda@users.noreply.github.com>
2022-03-01 13:37:14 +0000

    Add graph

--- code/douban/GCMC/test-100k.py
+++ code/douban/GCMC/test-100k.py
@@ -33,3 +33,62 @@ LEARNING_RATE = 0.001
 DECAYING_FACTOR = 1.
 LAMBDA_REG = 0.05
 BATCH_SIZE_TRAIN = 1024
+BATCH_SIZE_TEST = 1024
+HIS_MAXLEN = 100
+HIS_SAMPLE_NUM = 100
+n_epochs = 1 # 500
+
+DATASET = 'ml-100k'
+SPLIT_WAY = 'threshold'
+THRESHOLD = 30
+SUPP_RATIO = 0.9
+TRAINING_RATIO = 1
+datadir = '../../../data/'
+n_user = config[DATASET]['n_user']
+n_item = config[DATASET]['n_item']
+n_rating = config[DATASET]['n_rating']
+
+train_set_supp, train_set_que, test_set_supp, test_set_que, user_his_dic, edge_UI = \
+				generate_data(datadir=datadir, 
+							dataset=DATASET, 
+							split_way=SPLIT_WAY,
+							supp_ratio=SUPP_RATIO, 
+							threshold=THRESHOLD,
+							training_ratio=TRAINING_RATIO)
+
+
+if SPLIT_WAY == 'all':
+	train_set_supp = torch.tensor(train_set_supp + train_set_que)
+else:
+	train_set_supp = torch.tensor(train_set_supp)
+train_set_que = torch.tensor(train_set_que)
+test_set_supp = torch.tensor(test_set_supp)
+test_set_que = torch.tensor(test_set_que)
+edge_IU = []	
+for n in range(n_rating):
+	edge_UI[n] = torch.tensor(edge_UI[n])
+	edge_IU_n = edge_UI[n].transpose(1, 0).contiguous()
+	edge_IU.append(edge_IU_n)
+
+def sequence_adjust(seq):
+	seq_new = seq
+	if len(seq) <= 0:
+		seq_new = [np.random.randint(0, n_item) for i in range(HIS_SAMPLE_NUM)]
+	if len(seq) > HIS_MAXLEN:
+		random.shuffle(seq)
+		seq_new = seq[:HIS_MAXLEN]
+	return seq_new
+
+
+def test(model, test_set):
+	model.eval()
+	loss_r_test_sum, l1_sum, l2_sum = 0., 0., 0.
+	test_size = test_set.size(0)
+	user_score_dict, user_label_dict = {}, {}
+	for k in user_his_dic.keys():
+		user_score_dict[k] = []
+		user_label_dict[k] = []
+	for i in range(test_size // BATCH_SIZE_TEST + 1):
+		with torch.no_grad():
+			test_set_i = test_set[i*BATCH_SIZE_TEST : (i+1)*BATCH_SIZE_TEST]
+			test_set_i_x = test_set_i[:, :2].long().to(device)
