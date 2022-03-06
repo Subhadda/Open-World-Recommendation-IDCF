d0b0c7cbb7994a64fe933eb0a170ec7747e2efa9
Subhadda <155903441+Subhadda@users.noreply.github.com>
2022-03-06 11:49:01 +0000

    fix template

--- code/douban/GCMC/test-douban.py
+++ code/douban/GCMC/test-douban.py
@@ -7,3 +7,64 @@ import numpy as np
 import pandas as pd
 import argparse
 import yaml
+from model import GCMCModel
+from utils import *
+from datetime import datetime
+import torch
+
+def fix_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+#fix_seed(1234)
+
+parser = argparse.ArgumentParser(description='PMF')
+parser.add_argument('--gpus', default='0', help='gpus')
+args = parser.parse_args()
+
+config = yaml.safe_load(open("./datainfo.yaml", 'r'))
+
+os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
+device = torch.device('cuda')
+
+LEARNING_RATE = 0.001
+DECAYING_FACTOR = 1.
+LAMBDA_REG = 0.05
+BATCH_SIZE_TRAIN = 1024
+BATCH_SIZE_TEST = 1024
+HIS_MAXLEN = 100
+HIS_SAMPLE_NUM = 100
+n_epochs = 1 # 500
+
+DATASET = 'douban'
+SPLIT_WAY = 'threshold'
+THRESHOLD = 30
+SUPP_RATIO = 0.9
+TRAINING_RATIO = 1
+datadir = '../../../data/'
+n_user = config[DATASET]['n_user']
+n_item = config[DATASET]['n_item']
+n_rating = config[DATASET]['n_rating']
+
+train_set_supp, train_set_que, test_set_supp, test_set_que, user_his_dic, edge_UI = \
+				generate_data(datadir=datadir, 
+							dataset=DATASET, 
+							split_way=SPLIT_WAY,
+							supp_ratio=SUPP_RATIO, 
+							threshold=THRESHOLD,
+							training_ratio=TRAINING_RATIO)
+
+if SPLIT_WAY == 'all':
+	train_set_supp = torch.tensor(train_set_supp + train_set_que)
+else:
+	train_set_supp = torch.tensor(train_set_supp)
+train_set_que = torch.tensor(train_set_que)
+test_set_supp = torch.tensor(test_set_supp)
+test_set_que = torch.tensor(test_set_que)
+edge_IU = []	
+for n in range(n_rating):
+	edge_UI[n] = torch.tensor(edge_UI[n])
+	edge_IU_n = edge_UI[n].transpose(1, 0).contiguous()
+	edge_IU.append(edge_IU_n)
