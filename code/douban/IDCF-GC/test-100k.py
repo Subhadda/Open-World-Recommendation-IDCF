a7b7bc84ba41c377033cb1802c49986fa617c021
Subhadda <155903441+Subhadda@users.noreply.github.com>
2022-04-19 08:03:03 +0000

    bump version to 1.0.1

---
+++ code/douban/IDCF-GC/test-100k.py
@@ -0,0 +1,52 @@
+
+import os
+import time
+import random
+import pickle
+import numpy as np
+import pandas as pd
+import argparse
+import yaml
+from model import IRMC_GC_Model, GCMCModel
+from utils import *
+from datetime import datetime
+import torch
+
+def fix_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+fix_seed(1234)
+
+parser = argparse.ArgumentParser(description='PMF')
+parser.add_argument('--gpus', default='0', help='gpus')
+parser.add_argument('--extra', action="store_true", help='whether extra or inter')
+args = parser.parse_args()
+
+config = yaml.safe_load(open("./datainfo.yaml", 'r'))
+
+os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
+device = torch.device('cuda')
+
+LEARNING_RATE = 0.001
+DECAYING_FACTOR = 1.
+LAMBDA_REG = 0.05
+BATCH_SIZE_TRAIN = 1024
+BATCH_SIZE_TEST = 1024
+HIS_MAXLEN = 100
+HIS_SAMPLE_NUM = 20
+n_epochs = 1 # 500
+
+DATASET = 'ml-100k'
+SPLIT_WAY = 'threshold'
+THRESHOLD = 30
+SUPP_RATIO = 0.8
+TRAINING_RATIO = 1
+EXTRA = args.extra
+
+datadir = '../../../data/'
+n_user = config[DATASET]['n_user']
+n_item = config[DATASET]['n_item']
+n_rating = config[DATASET]['n_rating']
