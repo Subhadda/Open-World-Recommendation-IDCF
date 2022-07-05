61db7002314b4bb859f39e225150b9d1b828551b
Subhadda <155903441+Subhadda@users.noreply.github.com>
2022-07-05 13:57:08 +0000

    Fix typo in comments.

--- code/douban/IDCF-GC/test-douban.py
+++ code/douban/IDCF-GC/test-douban.py
@@ -51,4 +51,7 @@ n_item = config[DATASET]['n_item']
 n_rating = config[DATASET]['n_rating']
 
 train_set_supp, train_set_que, test_set_supp, test_set_que, user_supp_list, user_his_dic, edge_UI = \
-generate_data(datadir=data
+generate_data(datadir=datadir, 
+				dataset=DATASET, 
+				split_way=SPLIT_WAY,
+				supp_r
