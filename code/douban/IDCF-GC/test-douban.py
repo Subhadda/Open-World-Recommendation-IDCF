7ed5c8faf348d19fa742c9c5c1c97d0fdcfce73e
Subhadda <155903441+Subhadda@users.noreply.github.com>
2022-08-30 19:27:30 +0000

    Add entry to CHANGELOG

--- code/douban/IDCF-GC/test-douban.py
+++ code/douban/IDCF-GC/test-douban.py
@@ -78,4 +78,7 @@ for n in range(n_rating):
 def sequence_adjust(seq):
 	seq_new = seq
 	if len(seq) <= 0:
-		seq_new = [np.random.randint(0, n_item) for i in range(HIS_SAMPLE_NUM
+		seq_new = [np.random.randint(0, n_item) for i in range(HIS_SAMPLE_NUM)]
+	if len(seq) > HIS_MAXLEN:
+		random.shuffle(seq)
+		se
