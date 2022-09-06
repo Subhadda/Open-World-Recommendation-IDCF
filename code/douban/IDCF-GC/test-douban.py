22c079b475fb88437bdfe5670d681d1a780b61bb
Subhadda <155903441+Subhadda@users.noreply.github.com>
2022-09-06 20:41:42 +0000

    Add latest Ansible versions (2.6.1, 2.5.6, 2.4.6.0)

--- code/douban/IDCF-GC/test-douban.py
+++ code/douban/IDCF-GC/test-douban.py
@@ -91,4 +91,5 @@ def train(model, optimizer, i, supp_or_que):
 	if supp_or_que == 'supp':
 		train_set_supp_i = train_set_supp[i*BATCH_SIZE_TRAIN : (i+1)*BATCH_SIZE_TRAIN]
 		train_set_supp_i_x = train_set_supp_i[:, :2].long().to(device)
-		train_set_supp_i_y = train_set_supp_i[:, 
+		train_set_supp_i_y = train_set_supp_i[:, 2].float().to(device)
+		edge_UI_i = [edge_UI[n][train_set_supp_i_x[:, 
