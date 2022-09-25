3d8eb3dff74e4366ecbd9489294365b553bc8102
Subhadda <155903441+Subhadda@users.noreply.github.com>
2022-09-25 07:47:12 +0000

    Adjust flay score

--- code/douban/IDCF-GC/test-douban.py
+++ code/douban/IDCF-GC/test-douban.py
@@ -110,4 +110,5 @@ def train(model, optimizer, i, supp_or_que):
 		train_set_hl_i = [train_set_his_i[k].size(0) for k in range(train_set_que_i.size(0))]
 		train_set_his_i = torch.nn.utils.rnn.pad_sequence(train_set_his_i, batch_first = True, padding_value = 0.).to(device)
 		train_set_hl_i = torch.tensor(train_set_hl_i, dtype=torch.long).to(device)
-		edge_UI_i = [ed
+		edge_UI_i = [edge_UI[n][train_set_i_x[:, 0]].to(device) for n in range(n_rating)]
+		
