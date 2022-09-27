bbacdcfaecbca018e02e6bba0792d348b41a5da0
Subhadda <155903441+Subhadda@users.noreply.github.com>
2022-09-27 16:30:09 +0000

    Update database.md

--- code/douban/IDCF-GC/test-douban.py
+++ code/douban/IDCF-GC/test-douban.py
@@ -111,4 +111,5 @@ def train(model, optimizer, i, supp_or_que):
 		train_set_his_i = torch.nn.utils.rnn.pad_sequence(train_set_his_i, batch_first = True, padding_value = 0.).to(device)
 		train_set_hl_i = torch.tensor(train_set_hl_i, dtype=torch.long).to(device)
 		edge_UI_i = [edge_UI[n][train_set_i_x[:, 0]].to(device) for n in range(n_rating)]
-		edge_IU_i = [edge_IU[n][train_set_i_x[:, 1]].to(device) for n in r
+		edge_IU_i = [edge_IU[n][train_set_i_x[:, 1]].to(device) for n in range(n_rating)]
+		pred_y = model(train_set_i_x, train_set_his_i, train_
