d671d6702ba2ba77f6eba41abbe841e09b0aad49
Subhadda <155903441+Subhadda@users.noreply.github.com>
2022-10-09 11:57:28 +0000

    Fix Markdown

--- code/douban/IDCF-GC/test-douban.py
+++ code/douban/IDCF-GC/test-douban.py
@@ -130,4 +130,5 @@ def test(model, test_set, supp_or_que):
 			test_set_i = test_set[i*BATCH_SIZE_TEST : (i+1)*BATCH_SIZE_TEST]
 			test_set_i_x = test_set_i[:, :2].long().to(device)
 			test_set_i_y = test_set_i[:, 2].float().to(device)
-			test_set_hi
+			test_set_his_i = [torch.tensor(
+				sequence_adjust( user_his_dic[test
