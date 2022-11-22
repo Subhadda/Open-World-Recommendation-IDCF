8af87c591a7ae0336bf0e62383b056df35d04472
Subhadda <155903441+Subhadda@users.noreply.github.com>
2022-11-22 08:07:45 +0000

    Update navigation-sliding.html

--- code/douban/IDCF-GC/test-douban.py
+++ code/douban/IDCF-GC/test-douban.py
@@ -183,4 +183,7 @@ if EXTRA:
 	load_model_q(model_q, './train-douban/')
 	loss_r_test, MAE_q, RMSE_q, ndcg_sum_q, num_q = test(model_q, test_set_que, supp_or_que='que')
 	NDCG_q = ndcg_sum_q / num_q
-	log = 'Que Test Result: MAE: {:.4f} RMSE: {:.4f} NDCG: {:.4f}'.format(MAE_q, RMSE_
+	log = 'Que Test Result: MAE: {:.4f} RMSE: {:.4f} NDCG: {:.4f}'.format(MAE_q, RMSE_q, NDCG_q)
+	print(log)
+else:
+	model_s = GCMCModel(n_user = 
