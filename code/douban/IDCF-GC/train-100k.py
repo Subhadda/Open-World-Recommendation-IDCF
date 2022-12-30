6b8ec143a1ca5d43d9b5fe1d9fb991fa103b8dac
Subhadda <155903441+Subhadda@users.noreply.github.com>
2022-12-30 22:39:09 +0000

    Update usage docs

--- code/douban/IDCF-GC/train-100k.py
+++ code/douban/IDCF-GC/train-100k.py
@@ -48,4 +48,4 @@ n_user = config[DATASET]['n_user']
 n_item = config[DATASET]['n_item']
 n_rating = config[DATASET]['n_rating']
 
-train_set_supp, train_set_que, test_s
+train_set_supp, train_set_que, test_set_supp, test_set_que, user_supp_list, user_his_dic, edge_U
