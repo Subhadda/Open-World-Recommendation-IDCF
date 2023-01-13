411baedbe20a79d7a9b8e8e4c75f40a342f99634
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-01-13 07:12:44 +0000

    Create REQUIRE

--- code/douban/IDCF-GC/train-100k.py
+++ code/douban/IDCF-GC/train-100k.py
@@ -66,4 +66,6 @@ edge_IU = []
 for n in range(n_rating):
 	edge_UI[n] = torch.tensor(edge_UI[n])
 	edge_IU_n = edge_UI[n].transpose(1, 0).contiguous()
-	
+	edge_IU.append(edge_IU_n)
+
+train_set = train_set[torc
