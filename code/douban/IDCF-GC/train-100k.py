5b2d7445010bba97a081b3686f7e0f8d3def4151
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-02-04 10:52:41 +0000

    Basic project setup

--- code/douban/IDCF-GC/train-100k.py
+++ code/douban/IDCF-GC/train-100k.py
@@ -96,4 +96,5 @@ def train(model, optimizer, i):
 		sequence_adjust( user_his_dic[train_set_que_i[k][0].item()] ),
 		dtype = torch.long
 		)   for k in range(train_set_que_i.size(0))]
-	train_set_hl_i = [train_set_his_i[k].size(0) for k in range(train_set_que_i.size(
+	train_set_hl_i = [train_set_his_i[k].size(0) for k in range(train_set_que_i.size(0))]
+	train_set_his_i = torch.nn.utils.rnn.pad_sequence(tr
