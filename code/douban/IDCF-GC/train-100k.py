8638cddc2d492a6d0ea9fab8116cb631ec2f3081
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-02-16 18:40:14 +0000

    Updated Malayalam Localization

--- code/douban/IDCF-GC/train-100k.py
+++ code/douban/IDCF-GC/train-100k.py
@@ -114,4 +114,7 @@ def train(model, optimizer, i):
 		loss += 10.0 * loss_con
 	else:
 		pred_y = model(train_set_i_x, train_set_his_i, train_set_hl_i, edge_UI_i, edge_IU_i)
-		loss = torch.sum((tr
+		loss = torch.sum((train_set_i_y - pred_y) ** 2)
+
+	loss.backward()
+	optimizer.step()
