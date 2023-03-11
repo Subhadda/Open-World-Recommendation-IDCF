5cc258fbefb29b5d4a413b34986cd364f28d5dd2
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-03-11 17:43:16 +0000

    add missing bracket

--- code/douban/IDCF-GC/train-100k.py
+++ code/douban/IDCF-GC/train-100k.py
@@ -148,4 +148,7 @@ def save_model(model, path):
 	if EXTRA:
 		torch.save(model.state_dict(), path+'model-extra.pkl')
 	else:
-		torch.save(model.state_dict(), path+'model-inter
+		torch.save(model.state_dict(), path+'model-inter.pkl')
+
+def load_model(model, path):
+	model.load_embe
