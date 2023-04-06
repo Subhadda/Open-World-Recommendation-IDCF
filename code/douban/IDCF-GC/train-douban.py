4928a75b2ba696944719d21b3bccf6b198d0ade7
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-04-06 08:13:12 +0000

    ARM: 7489/1: errata: fix workaround for erratum #720789 on UP systems

--- code/douban/IDCF-GC/train-douban.py
+++ code/douban/IDCF-GC/train-douban.py
@@ -48,3 +48,71 @@ datadir = '../../../data/'
 n_user = config[DATASET]['n_user']
 n_item = config[DATASET]['n_item']
 n_rating = config[DATASET]['n_rating']
+
+train_set_supp, train_set_que, test_set_supp, test_set_que, user_supp_list, user_his_dic, edge_UI = \
+generate_data(datadir=datadir, 
+				dataset=DATASET, 
+				split_way=SPLIT_WAY,
+				supp_ratio=SUPP_RATIO, 
+				threshold=THRESHOLD,
+				training_ratio=TRAINING_RATIO)
+
+supp_users = torch.tensor(user_supp_list, dtype = torch.long)
+if EXTRA:
+	train_set = torch.tensor(train_set_supp)
+else:
+	train_set = torch.tensor(train_set_que)
+test_set = torch.tensor(test_set_que)
+edge_IU = []	
+for n in range(n_rating):
+	edge_UI[n] = torch.tensor(edge_UI[n])
+	edge_IU_n = edge_UI[n].transpose(1, 0).contiguous()
+	edge_IU.append(edge_IU_n)
+
+train_set = train_set[torch.randperm(train_set.size(0))]
+val_set = train_set[int(0.95 * train_set.size(0)):]
+if EXTRA:
+	pass
+else:
+	train_set = train_set[:int(0.95*train_set.size(0))]
+
+def sequence_adjust(seq):
+	seq_new = seq
+	if len(seq) <= 0:
+		seq_new = [np.random.randint(0, n_item) for i in range(HIS_SAMPLE_NUM)]
+	if len(seq) > HIS_MAXLEN:
+		random.shuffle(seq)
+		seq_new = seq[:HIS_MAXLEN]
+	return seq_new
+
+def train(model, optimizer, i):
+	model.train()
+	optimizer.zero_grad()
+	
+	train_set_que_i = train_set[i*BATCH_SIZE_TRAIN : (i+1)*BATCH_SIZE_TRAIN]
+	
+	train_set_i_x = train_set_que_i[:, :2].long().to(device)
+	train_set_i_y = train_set_que_i[:, 2].float().to(device)
+	train_set_his_i = [torch.tensor(
+		sequence_adjust( user_his_dic[train_set_que_i[k][0].item()] ),
+		dtype = torch.long
+		)   for k in range(train_set_que_i.size(0))]
+	train_set_hl_i = [train_set_his_i[k].size(0) for k in range(train_set_que_i.size(0))]
+	train_set_his_i = torch.nn.utils.rnn.pad_sequence(train_set_his_i, batch_first = True, padding_value = 0.).to(device)
+	train_set_hl_i = torch.tensor(train_set_hl_i, dtype=torch.long).to(device)
+	edge_UI_i = [edge_UI[n][train_set_i_x[:, 0]].to(device) for n in range(n_rating)]
+	edge_IU_i = [edge_IU[n][train_set_i_x[:, 1]].to(device) for n in range(n_rating)]
+
+	if EXTRA:
+		pred_y, user_emb_ind, user_emb_trd = model(train_set_i_x, train_set_his_i, train_set_hl_i, edge_UI_i, edge_IU_i, mode='EXTRA')
+		loss = torch.sum((train_set_i_y - pred_y) ** 2)
+		user_emb_trd_ = user_emb_trd.unsqueeze(0).repeat(user_emb_ind.size(0), 1, 1)
+		user_emb_ind_ = user_emb_ind.unsqueeze(1).repeat(1, user_emb_trd.size(0), 1)
+		dot_prod = torch.sum(torch.mul(user_emb_trd_, user_emb_ind_), dim=-1)
+		loss_con = - torch.mean(
+			dot_prod.diagonal() - torch.logsumexp(dot_prod, dim=-1)
+		)
+		loss += 10.0 * loss_con
+	else:
+		pred_y = model(train_set_i_x, train_set_his_i, train_set_hl_i, edge_UI_i, edge_IU_i)
+		loss = torch.sum((train_set_i_y - pred_y) ** 2)
