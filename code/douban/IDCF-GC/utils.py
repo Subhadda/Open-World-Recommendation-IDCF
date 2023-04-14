49b80cc19e71c646b07cb4b2b3f6513f2cb1750d
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-04-14 09:13:59 +0000

    release v2.0.2

--- code/douban/IDCF-GC/utils.py
+++ code/douban/IDCF-GC/utils.py
@@ -112,3 +112,53 @@ def generate_data(datadir, dataset='ml-1m', split_way='threshold', threshold=50,
                 test_set_que_u = []
                 for index, i in enumerate(test_ui_dic[u]):
                     test_set_que.append([u, i, test_ur_dic[u][index]])
+                user_que_num += 1
+                user_supp_list.append(u)
+    
+    user_his_dic = {}
+    for u in train_ui_dic.keys():
+        user_his_dic[u] = train_ui_dic[u]
+
+    edge_array = np.array([train_u, train_i, train_r], dtype=np.int32)
+    edge_UI = []
+    
+    for i in range(1, n_rating+1):
+        edge_i = edge_array[:2, edge_array[2]==i]
+        edge_UI_i = np.zeros((n_user, n_item), dtype=np.int)
+        edge_UI_i[edge_i[0], edge_i[1]] = 1
+        edge_UI.append(edge_UI_i)
+
+
+
+    print("-------Dataset Info--------")
+    if split_way == 'threshold':
+        print("split way [threshold] with threshold {} training_ratio {}".format(threshold, training_ratio))
+    if split_way == 'random':
+        print("split way [random] with supp_ratio {} training_ratio {}".format(supp_ratio, training_ratio))
+    if split_way == 'all':
+        print("split way [all] with threshold {} training_ratio {}".format(threshold, training_ratio))
+    print("support user {}, query user {}".format(user_supp_num, user_que_num))
+    print("train set size: support/query {}/{}".format(len(train_set_supp), len(train_set_que)))
+    print("test set size: support/query {}/{}".format(len(test_set_supp), len(test_set_que)))
+
+    return train_set_supp, train_set_que, test_set_supp, test_set_que, user_supp_list, user_his_dic, edge_UI
+
+def dcg_k(score_label, k):
+    dcg, i = 0., 0
+    for s in score_label:
+        if i < k:
+            dcg += (2**s[1]-1) / np.log2(2+i)
+            i += 1
+    return dcg
+
+def ndcg_k(y_hat, y, k):
+    score_label = np.stack([y_hat, y], axis=1).tolist()
+    score_label = sorted(score_label, key=lambda d:d[0], reverse=True)
+    score_label_ = sorted(score_label, key=lambda d:d[1], reverse=True)
+    norm, i = 0., 0
+    for s in score_label_:
+        if i < k:
+            norm += (2**s[1]-1) / np.log2(2+i)
+            i += 1
+    dcg = dcg_k(score_label, k)
+    return dcg / norm
