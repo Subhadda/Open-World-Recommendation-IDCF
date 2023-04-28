6c2bf7bd045970ce1696bb1fa1d478a622e8bb44
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-04-28 05:25:38 +0000

    Fixed build script.

---
+++ code/douban/IDCF-NN/test-douban.py
@@ -0,0 +1,13 @@
+
+import os
+import time
+import random
+import pickle
+import numpy as np
+import pandas as pd
+import argparse
+import yaml
+from model import IRMC_NN_Model, NNMFModel
+from utils import *
+from datetime import datetime
+import torch
