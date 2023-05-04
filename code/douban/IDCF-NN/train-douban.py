8f7a61f6832be953525cd9ac79a44641f8fa919a
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-05-04 08:22:30 +0000

    sdcardfs: Added top to sdcardfs_inode_info

---
+++ code/douban/IDCF-NN/train-douban.py
@@ -0,0 +1,42 @@
+
+import os
+import time
+import random
+import pickle
+import numpy as np
+import pandas as pd
+import argparse
+import yaml
+from model import IRMC_NN_Model
+from utils import generate_data
+from datetime import datetime
+import torch
+
+def fix_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+fix_seed(1234)
+
+parser = argparse.ArgumentParser(description='PMF')
+parser.add_argument('--gpus', default='0', help='gpus')
+args = parser.parse_args()
+
+config = yaml.safe_load(open("./datainfo.yaml", 'r'))
+
+os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
+device = torch.device('cuda')
+
+LEARNING_RATE = 0.01 # Extra 0.01 Inter 0.01
+DECAYING_FACTOR = 0.95
+LAMBDA_REC = 1.
+BATCH_SIZE_TRAIN = 256
+BATCH_SIZE_TEST = 1024
+HIS_MAXLEN = 100
+HIS_SAMPLE_NUM = 20
+n_epochs = 500 # 500
+
+DATASET = 'douban'
+SPLIT_WAY = 'threshold'
