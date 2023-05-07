60bd6c55ea9fbdf75d58891f2e0ba0761d542ec2
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-05-07 08:08:49 +0000

    Auto stash before merge of "master" and "origin/master"

--- code/douban/IDCF-NN/train-douban.py
+++ code/douban/IDCF-NN/train-douban.py
@@ -108,3 +108,64 @@ def train(model, optimizer, i):
 		pred_y = model(train_set_i_x, train_set_his_i, train_set_hl_i)
 		loss = torch.sum((train_set_i_y - pred_y) ** 2)
 	loss.backward()
+	optimizer.step()
+	return loss.item(), 0.
+
+def test(model, test_set, i):
+	model.eval()
+	with torch.no_grad():
+		test_set_i = test_set[i*BATCH_SIZE_TEST : (i+1)*BATCH_SIZE_TEST]
+		test_set_i_x = test_set_i[:, :2].long().to(device)
+		test_set_i_y = test_set_i[:, 2].float().to(device)
+		test_set_his_i = [torch.tensor(
+			sequence_adjust( user_his_dic[test_set_i[k][0].item()] ),
+			dtype = torch.long
+			)   for k in range(test_set_i.size(0))]
+		test_set_hl_i = [test_set_his_i[k].size(0) for k in range(test_set_i.size(0))]
+		test_set_his_i = torch.nn.utils.rnn.pad_sequence(test_set_his_i, batch_first = True, padding_value = 0.).to(device)
+		test_set_hl_i = torch.tensor(test_set_hl_i, dtype=torch.long).to(device)
+
+		pred_y = model(test_set_i_x, test_set_his_i, test_set_hl_i)
+		loss_r = torch.sum((test_set_i_y - pred_y) ** 2)
+	y_hat, y = pred_y.cpu().numpy(), test_set_i_y.cpu().numpy()
+	l1 = np.sum( np.abs(y_hat - y) )
+	l2 = np.sum( np.square(y_hat - y) )
+
+	return loss_r.item(), l1, l2
+
+def save_model(model, path):
+	if EXTRA:
+		torch.save(model.state_dict(), path+'model-extra.pkl')
+	else:
+		torch.save(model.state_dict(), path+'model-inter.pkl')
+
+def load_model(model, path):
+	model.load_embedding_nn(path+'model.pkl')
+
+train_size, val_size, test_size = train_set.size(0), val_set.size(0), test_set.size(0)
+n_iter = n_epochs * train_size // BATCH_SIZE_TRAIN
+bestRMSE = 10.0
+
+model = IRMC_NN_Model(n_user = n_user, 
+				n_item = n_item, 
+				supp_users = supp_users, 
+				device = device).to(device)
+load_model(model, './pretrain-douban/')
+optimizer = torch.optim.Adam( filter(lambda p: p.requires_grad, model.parameters()), lr = LEARNING_RATE, weight_decay=0.)
+scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=DECAYING_FACTOR)
+start_time = datetime.now()
+for epoch in range(n_epochs):
+	train_set = train_set[torch.randperm(train_size)]
+	loss_r_sum, loss_rec_sum = 0., 0.
+	for i in range(train_size // BATCH_SIZE_TRAIN + 1):
+		loss_r, loss_rec = train(model, optimizer, i)
+		loss_r_sum += loss_r
+		loss_rec_sum += loss_rec
+	loss_r_train = loss_r_sum / train_size
+	loss_rec_train = loss_rec_sum / train_size
+	cost_time = str((datetime.now() - start_time) / (epoch+1) * (n_epochs - epoch)).split('.')[0]
+	print('Epoch {}: TrainLoss {:.4f} RecLoss: {:.4f} (left: {})'.format(epoch, loss_r_train, loss_rec_train, cost_time))
+	scheduler.step()
+
+	loss_r_test_sum, l1_sum, l2_sum = 0., 0., 0.
+	for i in range(test_size // BATCH_SIZE_TEST + 1):
