bfe9ff3a5f67095e1173947f374807d3902d4bd7
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-06-03 18:29:28 +0000

    Automated merge

--- code/ml-1m/GCMC/test-1m.py
+++ code/ml-1m/GCMC/test-1m.py
@@ -9,3 +9,70 @@ import argparse
 import yaml
 from model import GCMCModel
 from utils import *
+from datetime import datetime
+import torch
+
+def fix_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+fix_seed(1234)
+
+parser = argparse.ArgumentParser(description='PMF')
+parser.add_argument('--gpus', default='0', help='gpus')
+args = parser.parse_args()
+
+config = yaml.safe_load(open("./datainfo.yaml", 'r'))
+
+os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
+device = torch.device('cuda')
+
+LEARNING_RATE = 0.001
+DECAYING_FACTOR = 1.
+LAMBDA_REG = 0.05
+BATCH_SIZE_TRAIN = 1024
+BATCH_SIZE_TEST = 1024
+HIS_MAXLEN = 100
+HIS_SAMPLE_NUM = 20
+n_epochs = 1 # 500
+
+DATASET = 'ml-1m'
+TRAINING_RATIO = 1
+THRESHOLD = 30
+datadir = '../../../data/'
+n_user = config[DATASET]['n_user']
+n_item = config[DATASET]['n_item']
+n_rating = config[DATASET]['n_rating']
+
+train_set_supp, train_set_que, test_set_supp, test_set_que, user_his_dic, edge_UI = \
+				generate_data(datadir=datadir, 
+							dataset=DATASET,
+							threshold=THRESHOLD,
+							training_ratio=TRAINING_RATIO,
+							sample_graph=False)
+
+train_set_que = torch.tensor(train_set_que)
+train_set_supp = torch.tensor(train_set_supp)
+test_set = torch.tensor(test_set_supp + test_set_que)
+test_set_supp = torch.tensor(test_set_supp)
+test_set_que = torch.tensor(test_set_que)
+edge_IU = []	
+for n in range(n_rating):
+	edge_UI[n] = torch.tensor(edge_UI[n])
+	edge_IU_n = edge_UI[n].transpose(1, 0).contiguous()
+	edge_IU.append(edge_IU_n)
+
+def train(model, optimizer, i):
+	model.train()
+	optimizer.zero_grad()
+
+	train_set_i = train_set_que[i*BATCH_SIZE_TRAIN : (i+1)*BATCH_SIZE_TRAIN]
+	train_set_i_x = train_set_i[:, :2].long().to(device)
+	train_set_i_y = train_set_i[:, 2].long().to(device)
+	edge_UI_i = [edge_UI[n][train_set_i_x[:, 0]].to(device) for n in range(n_rating)]
+	edge_IU_i = [edge_IU[n][train_set_i_x[:, 1]].to(device) for n in range(n_rating)]
+
+	pred_y = model(train_set_i_x, edge_UI_i, edge_IU_i)
+	loss_r = torch.sum((train_set_i_y - pred_y) ** 2)
