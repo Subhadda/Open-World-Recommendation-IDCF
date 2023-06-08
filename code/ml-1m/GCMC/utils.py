c372f3faff531cf72e91c77310b02be4facffec2
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-06-08 18:27:46 +0000

    Update coverage from 4.5.1 to 4.5.2

--- code/ml-1m/GCMC/utils.py
+++ code/ml-1m/GCMC/utils.py
@@ -9,4 +9,5 @@ config = yaml.safe_load(open("./datainfo.yaml", 'r'))
 
 def generate_data(datadir, dataset='ml-1m', threshold=30, training_ratio=1, sample_graph=False):
     n_user = config[dataset]['n_user']
-  
+    n_item = config[dataset]['n_item']
+    n_rating = config[dataset]['n_
