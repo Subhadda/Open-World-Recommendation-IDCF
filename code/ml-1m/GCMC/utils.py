7205c1f2098565149fd3e34947a4e483fc6d24e1
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-06-22 05:37:46 +0000

    ARM: 8517/1: ICST: avoid arithmetic overflow in icst_hz()

--- code/ml-1m/GCMC/utils.py
+++ code/ml-1m/GCMC/utils.py
@@ -24,4 +24,6 @@ def generate_data(datadir, dataset='ml-1m', threshold=30, training_ratio=1, samp
 
     index = [i for i in range(len(u))]
     random.shuffle(index)
-    train_index, test_index = index[:int(trainin
+    train_index, test_index = index[:int(training_ratio*0.9*len(u))], index[int(0.9*len(u)):]
+
+    train_ui_di
