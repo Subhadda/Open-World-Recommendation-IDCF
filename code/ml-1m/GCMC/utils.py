4dee2a897252dab1ca74cd19ec816fa828c4c442
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-07-26 15:41:22 +0000

    new blog

--- code/ml-1m/GCMC/utils.py
+++ code/ml-1m/GCMC/utils.py
@@ -57,4 +57,5 @@ def generate_data(datadir, dataset='ml-1m', threshold=30, training_ratio=1, samp
             for index, i in enumerate(train_ui_dic[u]):
                 train_set_supp.append([u, i, train_ur_dic[u][index]])
             test_set_supp_u = []
-            f
+            for index, i in enumerate(test_ui_dic[u]):
+            
