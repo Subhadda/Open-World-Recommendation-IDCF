c7e9e253bf263304457f086287a7c1168f05fe46
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-08-12 11:44:16 +0000

    txs auto checkin

--- code/ml-1m/GCMC/utils.py
+++ code/ml-1m/GCMC/utils.py
@@ -61,4 +61,5 @@ def generate_data(datadir, dataset='ml-1m', threshold=30, training_ratio=1, samp
                 test_set_supp.append([u, i, test_ur_dic[u][index]])
             user_supp_num += 1
         else:
-            for ind
+            for index, i in enumerate(train_ui_dic[u]):
+                trai
