8273130d08a786652c88c9a76bdb088472ad9f42
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-10-20 12:54:24 +0000

    Delete .README.md.swp

--- code/ml-1m/GCMC/utils.py
+++ code/ml-1m/GCMC/utils.py
@@ -102,4 +102,7 @@ def generate_data(datadir, dataset='ml-1m', threshold=30, training_ratio=1, samp
     print("split way [all] with training_ratio {}".format(training_ratio))
     print("support user {}, query user {}".format(user_supp_num, user_que_num))
     print("train set size: support/query {}/{}".format(len(train_set_supp), len(train_set_que)))
-    print("test set size: support/query {}/{}".format(len(test_set_supp), 
+    print("test set size: support/query {}/{}".format(len(test_set_supp), len(test_set_que)))
+
+    if sample_graph:
+        return train_set_s
