db8bc02de897e34e75e6e52906aec39c04d3a4be
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-11-01 22:14:21 +0000

    Edit readme file

--- code/ml-1m/GCMC/utils.py
+++ code/ml-1m/GCMC/utils.py
@@ -113,4 +113,8 @@ def dcg_k(score_label, k):
     dcg, i = 0., 0
     for s in score_label:
         if i < k:
-            dcg += (2**s[1]-1) 
+            dcg += (2**s[1]-1) / np.log2(2+i)
+            i += 1
+    return dcg
+
+def ndcg_k(y_hat, y, k)
