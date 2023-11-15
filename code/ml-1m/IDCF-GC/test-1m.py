76bf74a37244f9681ccaf9e7b97bccc9723de200
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-11-15 16:34:17 +0000

    sync: signal pt before sync_timeline object gets destroyed

--- code/ml-1m/IDCF-GC/test-1m.py
+++ code/ml-1m/IDCF-GC/test-1m.py
@@ -157,3 +157,59 @@ def test(model, test_set, supp_or_que):
 	MAE = l1_sum / test_size
 	RMSE = np.sqrt( l2_sum / test_size )
 	for k in user_score_dict.keys():
+		if len(user_score_dict[k]) <= 1:
+			continue
+		ndcg_sum += ndcg_k(user_score_dict[k], user_label_dict[k], len(user_score_dict[k]))
+		num += 1
+	return TestLoss, MAE, RMSE, ndcg_sum, num
+
+def load_model_s(model, path):
+	model.load_model(path+'model.pkl')
+
+def load_model_q(model, path):
+	if EXTRA:
+		model.load_model(path+'model-extra.pkl')
+	else:
+		model.load_model(path + 'model-inter.pkl')
+
+if EXTRA:
+	model_q = IRMC_GC_Model(n_user=n_user,
+							n_item=n_item,
+							n_rating=n_rating,
+							supp_users=supp_users,
+							device=device).to(device)
+	load_model_q(model_q, './train-1m/')
+
+	loss_r_test, MAE_q, RMSE_q, ndcg_sum_q, num_q = test(model_q, test_set_que, supp_or_que='que')
+	NDCG_q = ndcg_sum_q / num_q
+	log = 'Que Test Result: MAE: {:.4f} RMSE: {:.4f} NDCG: {:.4f}'.format(MAE_q, RMSE_q, NDCG_q)
+	print(log)
+else:
+	model_s = GCMCModel(n_user = n_user,
+					n_item = n_item,
+					n_rating = n_rating,
+					device = device).to(device)
+	load_model_s(model_s, './pretrain-1m/')
+	loss_r_test, MAE_s, RMSE_s, ndcg_sum_s, num_s = test(model_s, test_set_supp, supp_or_que='supp')
+	NDCG_s = ndcg_sum_s / num_s
+	log = 'Key Test Result: MAE: {:.4f} RMSE: {:.4f} NDCG: {:.4f}'.format(MAE_s, RMSE_s, NDCG_s)
+	print(log)
+
+
+	model_q = IRMC_GC_Model(n_user = n_user,
+					n_item = n_item,
+					n_rating = n_rating,
+					supp_users = supp_users,
+					device = device).to(device)
+	load_model_q(model_q, './train-1m/')
+	loss_r_test, MAE_q, RMSE_q, ndcg_sum_q, num_q = test(model_q, test_set_que, supp_or_que='que')
+	NDCG_q = ndcg_sum_q / num_q
+	log = 'Que Test Result: MAE: {:.4f} RMSE: {:.4f} NDCG: {:.4f}'.format(MAE_q, RMSE_q, NDCG_q)
+	print(log)
+
+	supp_size, que_size = test_set_supp.size(0), test_set_que.size(0)
+	MAE = ( MAE_s * supp_size + MAE_q * que_size )/ (supp_size+que_size)
+	RMSE = np.sqrt( (RMSE_s**2 * supp_size + RMSE_q**2 * que_size) / (supp_size+que_size))
+	NDCG = (ndcg_sum_q + ndcg_sum_s) / (num_q + num_s)
+	log = 'All Test Result: MAE: {:.4f} RMSE: {:.4f} NDCG: {:.4f}'.format(MAE, RMSE, NDCG)
+	print(log)
