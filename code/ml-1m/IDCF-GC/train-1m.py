a594fe57ba5e5060ca70fd0fcf01459f46c47f23
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-12-08 21:15:22 +0000

    - Added mat-table - Added xp-overlay-injector - Added xp-refirer-behavior - Improved sharing mechanism on xp-shared-behavior - Improved validation and added `validate` method on xp-form - Improved `parseURL` and `toURL` functions on expandjs - Replaced `toggles-target` with `behavior` attribute on xp-pressed-behavior - Minor bug fixes

--- code/ml-1m/IDCF-GC/train-1m.py
+++ code/ml-1m/IDCF-GC/train-1m.py
@@ -75,4 +75,4 @@ train_set = train_set[:int(0.95*train_set.size(0))]
 def sequence_adjust(seq):
 	seq_new = seq
 	if len(seq) <= 0:
-		seq_new = [np.random.
+		seq_new = [np.random.randint(0, n_item) for i in range(HIS_SAMPLE_NUM)]
