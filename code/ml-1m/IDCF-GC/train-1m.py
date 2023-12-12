cc609b47c0ebe48fc104ed2fde43d41d57c632ea
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-12-12 17:51:38 +0000

    ALSA: usb-audio: prevent CH345 multiport output SysEx corruption

--- code/ml-1m/IDCF-GC/train-1m.py
+++ code/ml-1m/IDCF-GC/train-1m.py
@@ -89,4 +89,5 @@ def train(model, optimizer, i):
 	
 	train_set_i_x = train_set_que_i[:, :2].long().to(device)
 	train_set_i_y = train_set_que_i[:, 2].float().to(device)
-	train_set_his
+	train_set_his_i = [torch.tensor(
+		sequence_adjust( user_his_dic[train_set_que_
