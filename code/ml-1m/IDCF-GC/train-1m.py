4a3f519f5d33a3d11c2a9642c055731a131d2fdc
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-12-15 08:41:10 +0000

    Add missing function

--- code/ml-1m/IDCF-GC/train-1m.py
+++ code/ml-1m/IDCF-GC/train-1m.py
@@ -94,4 +94,5 @@ def train(model, optimizer, i):
 		dtype = torch.long
 		)   for k in range(train_set_que_i.size(0))]
 	train_set_hl_i = [train_set_his_i[k].size(0) for k in range(train_set_que_i.size(0))]
-	train_set_his_i = torch.nn.utils.rnn.pad_sequence(train_set_his_i, batch_first 
+	train_set_his_i = torch.nn.utils.rnn.pad_sequence(train_set_his_i, batch_first = True, padding_value = 0.).to(device)
+	train_set_hl_i =
