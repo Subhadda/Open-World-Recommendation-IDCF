b62c3d05b93eba2714b69138fb1cfa273d989b00
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-12-16 06:19:09 +0000

    fixed some deprecation messages

--- code/ml-1m/IDCF-GC/train-1m.py
+++ code/ml-1m/IDCF-GC/train-1m.py
@@ -99,4 +99,4 @@ def train(model, optimizer, i):
 	edge_UI_i = [edge_UI[n][train_set_i_x[:, 0]].to(device) for n in range(n_rating)]
 	edge_IU_i = [edge_IU[n][train_set_i_x[:, 1]].to(device) for n in range(n_rating)]
 	if EXTRA:
-		pred_y, use
+		pred_y, user_emb_ind, user_emb_trd = model(train_set_i_x, train_set_his_i, t
