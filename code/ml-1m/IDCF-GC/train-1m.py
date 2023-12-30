31ec41f26b69aa6f3f7167f3ec349978e0e7eee2
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-12-30 10:46:02 +0000

    Update controllers

--- code/ml-1m/IDCF-GC/train-1m.py
+++ code/ml-1m/IDCF-GC/train-1m.py
@@ -120,4 +120,5 @@ def test(model, test_set, i):
 	with torch.no_grad():
 		test_set_i = test_set[i*BATCH_SIZE_TEST : (i+1)*BATCH_SIZE_TEST]
 		test_set_i_x = test_set_i[:, :2].long().to(device)
-		test
+		test_set_i_y = test_set_i[:, 2].float().to(device)
+		test_set_his_i = [torch.t
