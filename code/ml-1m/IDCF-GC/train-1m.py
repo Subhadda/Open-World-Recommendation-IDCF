b64d8b4ed9bf1aefd1e1a343b4f37b62480261df
Subhadda <155903441+Subhadda@users.noreply.github.com>
2023-12-30 19:33:16 +0000

    Adding instructions

--- code/ml-1m/IDCF-GC/train-1m.py
+++ code/ml-1m/IDCF-GC/train-1m.py
@@ -121,4 +121,5 @@ def test(model, test_set, i):
 		test_set_i = test_set[i*BATCH_SIZE_TEST : (i+1)*BATCH_SIZE_TEST]
 		test_set_i_x = test_set_i[:, :2].long().to(device)
 		test_set_i_y = test_set_i[:, 2].float().to(device)
-		test_set_his_i = [torch.t
+		test_set_his_i = [torch.tensor(
+			sequence_adjust( user_his_dic[test_set_i[k][0].
