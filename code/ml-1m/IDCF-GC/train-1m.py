8e1f1117e7f733fee23d7264a7a90dfb6fecf9e3
Subhadda <155903441+Subhadda@users.noreply.github.com>
2024-01-31 16:31:44 +0000

    Updating www/index.html

--- code/ml-1m/IDCF-GC/train-1m.py
+++ code/ml-1m/IDCF-GC/train-1m.py
@@ -158,4 +158,5 @@ model = IRMC_GC_Model(n_user = n_user,
 				supp_users = supp_users, 
 				device = device).to(device)
 load_model(model, './pretrain-1m/')
-optimizer = torch.optim.Adam( filter(lambda p: p.requires_grad, model.parameters()), lr = LEARNING_RATE, weight_decay=0.) #5
+optimizer = torch.optim.Adam( filter(lambda p: p.requires_grad, model.parameters()), lr = LEARNING_RATE, weight_decay=0.) #5e-2
+scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer
