09de536c0fe064de1f8bca13105b012cd23c57cc
Subhadda <155903441+Subhadda@users.noreply.github.com>
2024-02-18 05:24:22 +0000

    tracing: Don't call page_to_pfn() if page is NULL

---
+++ code/ml-1m/IDCF-NN/test-1m.py
@@ -0,0 +1,61 @@
+
+import os
+import time
+import random
+import pickle
+import numpy as np
+import pandas as pd
+import argparse
+import yaml
+from model import IRMC_NN_Model, NNMFModel
+from utils import *
+from datetime import datetime
+import torch
+
+def fix_seed(seed):
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+fix_seed(1234)
+
+parser = argparse.ArgumentParser(description='PMF')
+parser.add_argument('--gpus', default='0', help='gpus')
+parser.add_argument('--extra', action="store_true", help='whether extra or inter')
+args = parser.parse_args()
+
+config = yaml.safe_load(open("./datainfo.yaml", 'r'))
+
+os.environ["CUDA_VISIBLE_DEVICES"] = args.gpus
+device = torch.device('cuda')
+
+LEARNING_RATE = 0.001
+DECAYING_FACTOR = 1.
+LAMBDA_REG = 0.05
+BATCH_SIZE_TRAIN = 1024
+BATCH_SIZE_TEST = 128
+HIS_MAXLEN = 100
+HIS_SAMPLE_NUM = 20
+n_epochs = 1 # 500
+
+DATASET = 'ml-1m'
+SPLIT_WAY = 'threshold'
+THRESHOLD = 30
+SUPP_RATIO = 0.8
+TRAINING_RATIO = 1.0
+EXTRA = args.extra
+datadir = '../../../data/'
+n_user = config[DATASET]['n_user']
+n_item = config[DATASET]['n_item']
+
+train_set_supp, train_set_que, test_set_supp, test_set_que, user_supp_list, user_his_dic = \
+generate_data(datadir=datadir, 
+				dataset=DATASET, 
+				split_way=SPLIT_WAY,
+				supp_ratio=SUPP_RATIO, 
+				threshold=THRESHOLD,
+				training_ratio=TRAINING_RATIO)
+
+user_supp_num = len(user_supp_list)
+user_que_num = n_user - user_supp_num
