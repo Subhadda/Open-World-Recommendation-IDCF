42afb26a2a53d7a5d17fcd8a78a927694a10c0e7
Subhadda <155903441+Subhadda@users.noreply.github.com>
2024-02-24 17:50:27 +0000

    markdown format

--- code/ml-1m/IDCF-NN/utils.py
+++ code/ml-1m/IDCF-NN/utils.py
@@ -139,3 +139,13 @@ def dcg_k(score_label, k):
     return dcg
 
 def ndcg_k(y_hat, y, k):
+    score_label = np.stack([y_hat, y], axis=1).tolist()
+    score_label = sorted(score_label, key=lambda d:d[0], reverse=True)
+    score_label_ = sorted(score_label, key=lambda d:d[1], reverse=True)
+    norm, i = 0., 0
+    for s in score_label_:
+        if i < k:
+            norm += (2**s[1]-1) / np.log2(2+i)
+            i += 1
+    dcg = dcg_k(score_label, k)
+    return dcg / norm
