5af62a45920637b86649eb317b325440f3a639cc
Subhadda <155903441+Subhadda@users.noreply.github.com>
2024-02-25 20:48:27 +0000

    fix units

--- code/ml-1m/NNMF/utils.py
+++ code/ml-1m/NNMF/utils.py
@@ -5,4 +5,6 @@ import numpy as np
 
 random.seed(1234)
 
-config = yaml.safe_load(open("./datainfo.yaml", 'r')
+config = yaml.safe_load(open("./datainfo.yaml", 'r'))
+
+def generate_data(datadir, dataset='ml-1m', thre
