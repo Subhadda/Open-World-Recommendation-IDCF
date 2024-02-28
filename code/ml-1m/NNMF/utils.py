555194c96a8e24541249b5f6422ad96cce56487a
Subhadda <155903441+Subhadda@users.noreply.github.com>
2024-02-28 14:43:52 +0000

    fix package json

--- code/ml-1m/NNMF/utils.py
+++ code/ml-1m/NNMF/utils.py
@@ -8,4 +8,6 @@ random.seed(1234)
 config = yaml.safe_load(open("./datainfo.yaml", 'r'))
 
 def generate_data(datadir, dataset='ml-1m', threshold=30, training_ratio=1):
-    n_user = config[dataset]['n_user
+    n_user = config[dataset]['n_user']
+    n_item = config[dataset]['n_item']
+    with ope
