405565c9dd5a0ed4ad6ab6afa62ca69a58343cf7
Subhadda <155903441+Subhadda@users.noreply.github.com>
2024-03-13 22:41:32 +0000

    Bump ws from 5.2.2 to 5.2.3

--- code/ml-1m/NNMF/utils.py
+++ code/ml-1m/NNMF/utils.py
@@ -46,4 +46,5 @@ def generate_data(datadir, dataset='ml-1m', threshold=30, training_ratio=1):
         test_ur_dic[u[index]].append(r[index])
 
     user_supp_num, user_que_num = 0, 0
-    train_set_supp,
+    train_set_supp, test_set_supp = [], []
+    train_set_que, test_s
