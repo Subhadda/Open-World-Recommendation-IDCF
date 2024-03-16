196941769eb7a3b553025bfd25f1daed42764b1e
Subhadda <155903441+Subhadda@users.noreply.github.com>
2024-03-16 10:51:14 +0000

    Add sources.

--- code/ml-1m/NNMF/utils.py
+++ code/ml-1m/NNMF/utils.py
@@ -50,4 +50,5 @@ def generate_data(datadir, dataset='ml-1m', threshold=30, training_ratio=1):
     train_set_que, test_set_que = [], []
 
     for u in train_ui_dic.keys():
-        num =
+        num = len(train_ui_dic[u])
+        if num >= thres
