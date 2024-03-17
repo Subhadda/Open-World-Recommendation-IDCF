20cdb3cbb5cf02acfeadda6d5c310d34a2a957c8
Subhadda <155903441+Subhadda@users.noreply.github.com>
2024-03-17 07:38:26 +0000

    Formatted Code

--- code/ml-1m/NNMF/utils.py
+++ code/ml-1m/NNMF/utils.py
@@ -53,4 +53,6 @@ def generate_data(datadir, dataset='ml-1m', threshold=30, training_ratio=1):
         num = len(train_ui_dic[u])
         if num >= threshold:
             for index, i in enumerate(train_ui_dic[u]):
-                train_set_supp.append([u, i, train_ur_d
+                train_set_supp.append([u, i, train_ur_dic[u][index]])
+            test_set_supp_u = []
+            for 
