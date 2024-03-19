572152d7012179af1ed0bf11a9985b0e5059b683
Subhadda <155903441+Subhadda@users.noreply.github.com>
2024-03-19 19:26:58 +0000

    Update personal.md

--- code/ml-1m/NNMF/utils.py
+++ code/ml-1m/NNMF/utils.py
@@ -62,4 +62,4 @@ def generate_data(datadir, dataset='ml-1m', threshold=30, training_ratio=1):
             for index, i in enumerate(train_ui_dic[u]):
                 train_set_que.append([u, i, train_ur_dic[u][index]])
             test_set_que_u = []
- 
+            for index, i in enumerate(test_ui
